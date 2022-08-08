08fc03a36528ba5f2e3d5afae8f104e706eec000
yanir2007 <87027341+godmingty@users.noreply.github.com>
2022-08-08 13:29:10 +0300

    to a,ilaty

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 (function () {
   const second = 1000,
         minute = second * 60,
@@ -64,20 +66,7 @@ class Notifications {
 		}
 	}
 }
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+
+function conf(){
+  window.location.href='#announcement';
 }
-
-
